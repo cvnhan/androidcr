dbc6687b3ae11c62e16c5ff1f76eb3b2e0f8b162
NhanCao <caovannhan2002@gmail.com>
2015-09-14 17:39:35 +0700

    upgrade version

--- app/src/main/java/com/cvnhan/androidcr/dagger/module/YKMAModuleModel.java
+++ app/src/main/java/com/cvnhan/androidcr/dagger/module/YKMAModuleModel.java
@@ -4,6 +4,7 @@ package com.cvnhan.androidcr.dagger.module;
  */
 
 import android.app.Application;
+import android.util.Log;
 
 import com.cvnhan.androidcr.mvp.model.YKMAIModelApi;
 import com.cvnhan.androidcr.mvp.model.YKMAModelPlayer;
@@ -11,25 +12,82 @@ import com.cvnhan.androidcr.mvp.model.YKMAModelRadio;
 import com.google.gson.ExclusionStrategy;
 import com.google.gson.FieldAttributes;
 import com.google.gson.GsonBuilder;
+import com.squareup.okhttp.Interceptor;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+import java.security.cert.CertificateException;
 
 import javax.inject.Singleton;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import dagger.Module;
 import dagger.Provides;
 import io.realm.RealmObject;
-import retrofit.RestAdapter;
-import retrofit.converter.GsonConverter;
+import retrofit.GsonConverterFactory;
+import retrofit.Retrofit;
+import retrofit.RxJavaCallAdapterFactory;
 
 @Module
 public class YKMAModuleModel {
     public static final String BASE_URL = "http://vannhan.comuv.com";
+    private static final String TRUSTED_HOST = "test3.sunnypoint.jp";
+    private static OkHttpClient getUnsafeOkHttpClient() {
+        try {
+            // Create a trust manager that does not validate certificate chains
+            final TrustManager[] trustAllCerts = new TrustManager[]{
+                    new X509TrustManager() {
+                        @Override
+                        public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+                        }
+
+                        @Override
+                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                            return null;
+                        }
+                    }
+            };
+
+            // Install the all-trusting trust manager
+            final SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+
+            // Create an ssl socket factory with our all-trusting manager
+            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+            OkHttpClient okHttpClient = new OkHttpClient();
+            okHttpClient.setSslSocketFactory(sslSocketFactory);
+            okHttpClient.setHostnameVerifier((hostname, session) -> hostname.contains(TRUSTED_HOST));
+            okHttpClient.interceptors().add(new Interceptor() {
+                @Override
+                public Response intercept(Chain chain) throws IOException {
+                    Response response = chain.proceed(chain.request());
+                    Log.e("response", response.toString());
+                    // Do anything with response here
+
+                    return response;
+                }
+            });
+            return okHttpClient;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Provides
     @Singleton
     public YKMAIModelApi provideApiModel() {
-        RestAdapter restAdapter = new RestAdapter.Builder()
-                .setEndpoint(BASE_URL)
-                .setConverter(new GsonConverter(new GsonBuilder()
+        Retrofit restAdapter = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(new GsonBuilder()
                         .setExclusionStrategies(new ExclusionStrategy() {
                             @Override
                             public boolean shouldSkipField(FieldAttributes f) {
@@ -42,7 +100,8 @@ public class YKMAModuleModel {
                             }
                         })
                         .create()))
-                .setLogLevel(RestAdapter.LogLevel.FULL)
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .client(getUnsafeOkHttpClient())
                 .build();
         return restAdapter.create(YKMAIModelApi.class);
     }
