1fdf32f0ef6e6d548a01bf0c86a902b81535e04e
Nhan Cao <caovannhan2002@gmail.com>
2016-01-18 13:09:09 +0530

    clean dependency clean app

---
+++ app/src/main/java/com/cvnhan/androidcr/NCMAMainActivity.java
@@ -0,0 +1,11 @@
+package com.cvnhan.androidcr;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by NhanCao on 13-Sep-15.
+ */
+public class NCMAMainActivity extends AppCompatActivity{
+
+
+}
